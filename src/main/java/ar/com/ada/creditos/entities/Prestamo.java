5c251595caf6f06bda6c3f83d035b4bc2884e6fd
m.avilano@gmail.com <m.avilano@gmail.com>
2021-05-21 20:47:27 -0300

    Se crea la clase prestamo.

---
+++ src/main/java/ar/com/ada/creditos/entities/Prestamo.java
@@ -0,0 +1,23 @@
+package ar.com.ada.creditos.entities;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+
+public class Prestamo{
+
+    private int prestamoId;
+
+    private Cliente cliente;
+
+    private Date fecha;
+
+    private BigDecimal importe;
+
+    private int cuotas;
+
+    private Date fechaAlta;
+
+
+
+}
