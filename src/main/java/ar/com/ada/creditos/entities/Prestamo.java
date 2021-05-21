03142c7cc1031a7772d1ec455db4ddd635ac4c66
m.avilano@gmail.com <m.avilano@gmail.com>
2021-05-21 20:51:00 -0300

    Se generan getters y setters  para c/atributo de prestamo

--- src/main/java/ar/com/ada/creditos/entities/Prestamo.java
+++ src/main/java/ar/com/ada/creditos/entities/Prestamo.java
@@ -10,6 +10,54 @@ public class Prestamo{
 
     private Cliente cliente;
 
+    public int getPrestamoId() {
+        return prestamoId;
+    }
+
+    public void setPrestamoId(int prestamoId) {
+        this.prestamoId = prestamoId;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    public int getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(int cuotas) {
+        this.cuotas = cuotas;
+    }
+
+    public Date getFechaAlta() {
+        return fechaAlta;
+    }
+
+    public void setFechaAlta(Date fechaAlta) {
+        this.fechaAlta = fechaAlta;
+    }
+
     private Date fecha;
 
     private BigDecimal importe;
