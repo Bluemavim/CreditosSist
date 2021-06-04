f471b27a4324c950656bca0e0b999f418954d234
m.avilano@gmail.com <m.avilano@gmail.com>
2021-06-04 19:55:44 -0300

    Se crean getters y setters y las referencias a las columnas correspondientes en la clase Cancelacion.java.

--- src/main/java/ar/com/ada/creditos/entities/Cancelacion.java
+++ src/main/java/ar/com/ada/creditos/entities/Cancelacion.java
@@ -1,5 +1,6 @@
 package ar.com.ada.creditos.entities;
 import javax.persistence.*;
+import java.util.Date;
 
 import java.math.BigDecimal;
 
@@ -8,23 +9,62 @@ import java.math.BigDecimal;
 @Table(name="cancelacion")
 public class Cancelacion {
     
-    @Id
-    
-    
+    @Id  
     @Column(name="cancelacion_id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int cancelacionId;
 
-    @Column(name="prestamo_id")
-    private Date fecha;
+    @ManyToOne
+    @JoinColumn(name="prestamo_id", referencedColumnName = "prestamo_id")
+    private Prestamo prestamo;
+
     @Column(name="fecha_cancelacion")
+    private Date fechaCancelacion;
+
+    @Column(name="importe")
     private BigDecimal importe;
+
     @Column(name="cuota")
     private int cuotas;
 
+    public int getCancelacionId() {
+        return cancelacionId;
+    }
 
-    @ManyToOne // los JoinColumn van donde está la FK
-    @JoinColumn(name = "prestamo_id", referencedColumnName = "prestamo_id")
-    private Cliente cliente;
+    public void setCancelacionId(int cancelacionId) {
+        this.cancelacionId = cancelacionId;
+    }
+
+    public Prestamo getPrestamo() {
+        return prestamo;
+    }
+
+    public void setPrestamo(Prestamo prestamo) {
+        this.prestamo = prestamo;
+    }
+
+    public Date getFechaCancelacion() {
+        return fechaCancelacion;
+    }
+
+    public void setFechaCancelacion(Date fechaCancelacion) {
+        this.fechaCancelacion = fechaCancelacion;
+    }
+
+    public BigDecimal getImporte() {
+        return importe;
+    }
+
+    public void setImporte(BigDecimal importe) {
+        this.importe = importe;
+    }
+
+    public int getCuotas() {
+        return cuotas;
+    }
+
+    public void setCuotas(int cuotas) {
+        this.cuotas = cuotas;
+    }
     
 }
